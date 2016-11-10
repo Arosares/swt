aa5ee7e91602405c30f5a62f47ed477b80ad99e5
Simon Meyer <simon.meyer@stud.uni-bamberg.de>
2016-11-10 20:00:57 +0100

    initial implementation of TestedClass and refining of UnitTest

--- TDA/src/tda/src/logic/UnitTest.java
+++ TDA/src/tda/src/logic/UnitTest.java
@@ -1,85 +1,85 @@
 package tda.src.logic;
 
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 
 public class UnitTest {
-/**
- * <pre>
- *           0..*     0..*
- * Test ------------------------- TestedClass
- *           test        &lt;       testedClass
- * </pre>
- */
-private Set<TestedClass> testedClass;
+	/**
+	 * <pre>
+	 *           0..*     0..*
+	 * Test ------------------------- TestedClass
+	 *           test        &lt;       testedClass
+	 * </pre>
+	 */
+	private Set<TestedClass> testedClass;
 
-public Set<TestedClass> getTestedClass() {
-   if (this.testedClass == null) {
-this.testedClass = new HashSet<TestedClass>();
-   }
-   return this.testedClass;
-}
+	public Set<TestedClass> getTestedClass() {
+		if (this.testedClass == null) {
+			this.testedClass = new HashSet<TestedClass>();
+		}
+		return this.testedClass;
+	}
 
-/**
- * <pre>
- *           0..*     0..*
- * Test ------------------------- Test
- *           test1        &lt;       test
- * </pre>
- */
-private Set<UnitTest> test;
+	/**
+	 * <pre>
+	 *           0..*     0..*
+	 * Test ------------------------- Test
+	 *           test1        &lt;       test
+	 * </pre>
+	 */
+	private Set<UnitTest> test;
 
-public Set<UnitTest> getTest() {
-   if (this.test == null) {
-this.test = new HashSet<UnitTest>();
-   }
-   return this.test;
-}
+	public Set<UnitTest> getTest() {
+		if (this.test == null) {
+			this.test = new HashSet<UnitTest>();
+		}
+		return this.test;
+	}
 
-/**
- * <pre>
- *           0..*     0..*
- * Test ------------------------- Test
- *           test        &gt;       test1
- * </pre>
- */
-private Set<UnitTest> test1;
+	/**
+	 * <pre>
+	 *           0..*     0..*
+	 * Test ------------------------- Test
+	 *           test        &gt;       test1
+	 * </pre>
+	 */
+	private Set<UnitTest> test1;
 
-public Set<UnitTest> getTest1() {
-   if (this.test1 == null) {
-this.test1 = new HashSet<UnitTest>();
-   }
-   return this.test1;
-}
+	public Set<UnitTest> getTest1() {
+		if (this.test1 == null) {
+			this.test1 = new HashSet<UnitTest>();
+		}
+		return this.test1;
+	}
 
-private String testName;
+	private String testName;
 
-public void setTestName(String value) {
-   this.testName = value;
-}
+	public void setTestName(String value) {
+		this.testName = value;
+	}
 
-public String getTestName() {
-   return this.testName;
-}
+	public String getTestName() {
+		return this.testName;
+	}
 
-private int executionID;
+	private String executionID;
 
-public void setExecutionID(int value) {
-   this.executionID = value;
-}
+	public void setExecutionID(String value) {
+		this.executionID = value;
+	}
 
-public int getExecutionID() {
-   return this.executionID;
-}
+	public String getExecutionID() {
+		return this.executionID;
+	}
 
-private boolean outcome;
+	private boolean outcome;
 
-public void setOutcome(boolean value) {
-   this.outcome = value;
-}
+	public void setOutcome(boolean value) {
+		this.outcome = value;
+	}
 
-public boolean isOutcome() {
-   return this.outcome;
-}
+	public boolean getOutcome() {
+		return this.outcome;
+	}
 
 }
