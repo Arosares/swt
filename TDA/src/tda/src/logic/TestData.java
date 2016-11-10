114201b33a63fb37a6befd921a78aaabc4104816
UB-JM <jan.martin@stud.uni-bamberg.de>
2016-11-10 19:26:42 +0100

    Added the TestData.java class
    Including the lists containing all already parsed TestRuns, Classes, and
    Tests.

---
+++ TDA/src/tda/src/logic/TestData.java
@@ -0,0 +1,45 @@
+package tda.src.logic;
+
+import java.util.ArrayList;
+
+public class TestData {
+	
+	
+//	Lists of already parsed Testruns
+	ArrayList<String> testedClassList = new ArrayList<String>();
+	
+	ArrayList<String> testRunList = new ArrayList<String>();
+	
+	ArrayList<String> testList = new ArrayList<String>();
+	
+//	Checks the testedClassList for the currently parsed Class. Returns boolean.
+	public boolean checkTestedClassListContent(String parsedClass) {
+		return testedClassList.contains(parsedClass);
+	}
+//	Checks the testRunList for the Testrun currently being parsed. Returns Boolean.
+	public boolean checkTestRunListContent(String parsedTestRunID){
+		return testRunList.contains(parsedTestRunID.toString());
+	}
+//	Checks the TestList for the currently parsed Test. Returns boolean. High performance Impact.
+	public boolean checkTestListContent(String parsedTest) {
+		return testList.contains(parsedTest);
+	}
+	
+	
+//	Adds a new entry to the List with the name of a newly parsed Class.
+	public void addToTestedClassList(String parsedClassName) {
+		testedClassList.add(parsedClassName);
+	}
+	
+//	Adds a new entry to the TestRunList with the ID of a new Test Run.
+	public void addToTestRunList(String parsedTestRun) {
+		testRunList.add(parsedTestRun);
+	}
+//	Add a newly found Test to the TestList.
+	public void addToTestList(String parsedTest) {
+		testRunList.add(parsedTest);
+	}
+
+	
+	
+}
