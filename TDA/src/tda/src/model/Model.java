ae5cbe0aed5583130c2402e7fc4c53738fcd0785
Frank Kessler <frank-phillip.kessler@stud.uni-bamberg.de>
2016-11-15 16:32:02 +0100

    First Linking in MVC

--- TDA/src/tda/src/model/Model.java
+++ TDA/src/tda/src/model/Model.java
@@ -1,43 +1,52 @@
 package tda.src.model;
+
 import java.util.Set;
 
 import tda.src.logic.Parser;
 import tda.src.logic.TestRun;
 
 import java.util.HashSet;
+import java.util.Observable;
 
-public class Model {
-/**
- * <pre>
- *           1..1     1..1
- * Model ------------------------> Parser
- *           model        &gt;       parser
- * </pre>
- */
-private Parser parser;
+public class Model extends Observable {
+	/**
+	 * <pre>
+	 *           1..1     1..1
+	 * Model ------------------------> Parser
+	 *           model        &gt;       parser
+	 * </pre>
+	 */
+	private Parser parser;
 
-public void setParser(Parser value) {
-   this.parser = value;
-}
+	public Model() {
+		super();
+		//initialize program:
+		
+	}
+	
+	
+	public void setParser(Parser value) {
+		this.parser = value;
+	}
 
-public Parser getParser() {
-   return this.parser;
-}
+	public Parser getParser() {
+		return this.parser;
+	}
 
-/**
- * <pre>
- *           1..1     0..*
- * Model ------------------------> TestRun
- *           model        &gt;       testRun
- * </pre>
- */
-private Set<TestRun> testRun;
+	/**
+	 * <pre>
+	 *           1..1     0..*
+	 * Model ------------------------> TestRun
+	 *           model        &gt;       testRun
+	 * </pre>
+	 */
+	private Set<TestRun> testRun;
 
-public Set<TestRun> getTestRun() {
-   if (this.testRun == null) {
-this.testRun = new HashSet<TestRun>();
-   }
-   return this.testRun;
-}
+	public Set<TestRun> getTestRun() {
+		if (this.testRun == null) {
+			this.testRun = new HashSet<TestRun>();
+		}
+		return this.testRun;
+	}
 
 }
