2ef4ef397956639e663330f3228b10dc855335fe
Frank Kessler <frank-phillip.kessler@stud.uni-bamberg.de>
2016-11-17 20:04:19 +0100

    imported project

--- TDA/test/AllTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ GameTests.class, InventoryTests.class })
-public class AllTests {
-
-}
